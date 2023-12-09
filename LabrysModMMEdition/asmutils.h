f6ae09d1e9670713e447fd3da85bbb4dba8d4bbf
labreezy <yourtooslow@gmail.com>
2023-12-09 09:17:00 -0600

    nuclear

--- LabrysModMMEdition/asmutils.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-bool Hook(void* toHook, void* ourFunc, int len);
--- LabrysModMMEdition/emeraldset.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-#include "SA2ModLoader.h"
-
-void LoadSetsFromFile(std::string fpath);
-void initSetHook();
-DataPointer(char, NumDeathsRestarts, 0x1934be8);
